7f9ade6445a63bdf48f8887ebae22a53193144e6
caophuccp <cna0x11@gmail.com>
2021-01-15 19:48:08 +0700

    init project

---
+++ ReviewSchool/ReviewSchool/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ReviewSchool
+//
+//
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
